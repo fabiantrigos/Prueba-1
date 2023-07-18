88cd08b51f12bfc38edf1932370236e2fd0bbc2b
fabiantrigos <fabiantrigos@gmail.com>
2023-07-18 00:52:59 -0500

    creando los tres archivos principales

---
+++ funciones.js
@@ -0,0 +1,20 @@
+let function alquiler(tipoDeVehiculo, diasDeAlquiler, sillaParaBebe) {
+    let compacto;
+    let mediano;
+    let camioneta;
+    let silla= 1200;
+    let dias;
+    let valorVehiculo;
+
+        if(tipoDeVehiculo = compacto){
+          valorVehiculo=14000; 
+        }
+        else if(tipoDeVehiculo= mediano){
+            valorVehiculo= 17000;
+        }
+        else (tipoDeVehiculo=camioneta){
+            valorVehiculo=28000;
+        }
+    return ()
+}
+console.log(alquiler(compacto));
---
+++ variables.js
@@ -0,0 +1,13 @@
+var nombre = 'Fabian';
+
+console.log (nombre);
+let numero_preferido = 15;
+
+console.log(numero_preferido);
+if(true){
+    let numero_numero_preferido = 29;
+    console.log(numero_numero_preferido);   
+
+}
+const apellido ='Trigos';
+console.log (apellido);
